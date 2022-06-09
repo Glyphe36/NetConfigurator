326d730dc052fafce06406989dbcab6cbdfde035
Jessy <jrobin68@gmail.com>
2022-06-09 19:49:28 +0200

    v1

--- NetConfigurtor/Form1.cs
+++ NetConfigurtor/Form1.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Diagnostics;
 using System.Management;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
 
 namespace NetConfigurtor
 {
